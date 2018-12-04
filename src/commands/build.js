f68ffe8bdbf8eed839e28d3794996af8e61186fe
Branden Cash <bcash@parchment.com>
2018-12-04 10:47:56 -0700

    fix up build/test stuff

--- src/commands/build.js
+++ src/commands/build.js
@@ -1,16 +1,16 @@
 import exec from 'executive';
 
-export default async ({ path: buildDir = process.cwd(), registry, image, tagVersion, tagBranch, tag }) => {
-    console.log('path', buildDir);
-    console.log('registry', registry);
-    console.log('image', image);
-    console.log('tagVersion', tagVersion);
-    console.log('tagBranch', tagBranch);
-    console.log('tag', tag);
+export default async ({ path: cwd = process.cwd(), registry, image, tagVersion, tagBranch, tag }) => {
+    // console.log('path', cwd);
+    // console.log('registry', registry);
+    // console.log('image', image);
+    // console.log('tagVersion', tagVersion);
+    // console.log('tagBranch', tagBranch);
+    // console.log('tag', tag);
 
     // docker build
     const tags = tag.map(tag => `--tag ${registry}/${image}:${tag}`).join(' ');
-    const { status, stdout, stderr } = await exec.strict(`docker build ${tags} .`, { cwd: path });
+    const { status, stdout, stderr } = await exec.strict(`docker build ${tags} .`, { cwd });
 
 
 }
